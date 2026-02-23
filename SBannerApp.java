2751dc3dabddb8877cd67dd4f23787fae631511c
Sonu <sb0206@srmist.edu.in>
2026-02-23 13:53:58 +0530

    Coded to print oops using string

---
+++ SBannerApp.java
@@ -0,0 +1,16 @@
+public class SBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ","   ***        ***      ******        ***** "));
+        System.out.println(String.join(" "," **   **    **   **    **    **     **     "));
+        System.out.println(String.join(" ","**     **  **     **   **     **   **     "));
+        System.out.println(String.join(" ","**     **  **     **   **    **     **  "));
+        System.out.println(String.join(" ","**     **  **     **   ******        ***"));
+        System.out.println(String.join(" ","**     **  **     **   **              **  "));
+        System.out.println(String.join(" ","**     **  **     **   **               **     "));
+        System.out.println(String.join(" "," **   **    **    **   **              **       "));
+        System.out.println(String.join(" ","   ***        ***      **         *****    "));
+
+    }
+}
